2772cc4cdb8a5ffa514ea7e623a89c9adb869b17
Dennis Sørensen <dennissorensen24@hotmail.dk>
2020-02-13 22:31:56 +0100

    Leg med forskellige contetview, og opsætning af app

--- FindAndBoxIt/ContentView.swift
+++ FindAndBoxIt/ContentView.swift
@@ -8,14 +8,24 @@
 
 import SwiftUI
 
+//Til at opbygge selve viewet
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        Text("Hello, World!, it is working")
     }
 }
 
+//For at kunne generere det output der vises i previewet
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        
+        Group {
+            ContentView().colorScheme(.light).previewDisplayName("iPhone 11 Light mode").previewDevice("iPhone 11")
+            
+            ContentView().colorScheme(.dark).previewDisplayName("iPhone 11 Dark mode").previewDevice("iPhone 11")
+            
+            ContentView().previewDevice("iPhone SE").previewDisplayName("iPhone SE")
+        }
+        
     }
 }
