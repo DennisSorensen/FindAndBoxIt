00362f1fd6ec691a1cfa88d6c46137ffed0d4505
Dennis Sørensen <dennissorensen24@hotmail.dk>
2020-04-07 17:05:19 +0200

    Added colors

---
+++ FindAndBoxIt/Extenstions/Color+BrandColors.swift
@@ -0,0 +1,17 @@
+//
+//  Color+BrandColors.swift
+//  FindAndBoxIt
+//
+//  Created by Dennis Sørensen on 07/04/2020.
+//  Copyright © 2020 Dennis Sørensen. All rights reserved.
+//
+
+import SwiftUI
+
+extension Color {
+    static let brandMain = Color("BrandMain")
+    static let brandLight = Color("BrandLight")
+    static let brandAction = Color("BrandAction")
+    static let brandLittleDark = Color("BrandLittleDark")
+    static let brandDark = Color("BrandDark")
+}
